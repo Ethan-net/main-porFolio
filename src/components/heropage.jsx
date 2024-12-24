ae952c806111fc82fa10a2ae040ef4f5988b3e9f
Ethan-net <koredeajibola@gmail.com>
2024-12-24 11:33:10 +0100

    update

--- src/components/heropage.jsx
+++ src/components/heropage.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import '../App.css'
 import myImg from '../assets/images/mesmiling.jpeg'
-import arrow from '../assets/icons/arrow.svg'
+import linkdn from '../assets/icons/linkdnic.svg'
+import instagram from '../assets/icons/instaicon.svg'
+import facebook from '../assets/icons/facebookicon.svg'
+import twitter from '../assets/icons/twitericon.svg'
 import download from '../assets/icons/download.png'
 import { Link } from 'react-router-dom';
 
@@ -11,28 +14,28 @@ export default function Heropage() {
       <div className='bg-black/80'>
         <section className='justify-center p-10'>
         <div className="list-wrapper text-center items-center">
-          <h1 className="text-white text-lg sm:text-xl md:text-2xl mb-10">
+          <h1 className="text-white font-bold text-lg sm:text-xl md:text-2xl mb-10">
             Hi...
           </h1>
-          <h1 className="text-white text-lg sm:text-xl md:text-2xl my-5">
+          <h1 className="text-white text-lg font-bold sm:text-xl md:text-2xl my-5">
             
               I am{"  "}
               <strong className="text-yellow-600 text-[30px] ">Ajibola Korede Ayo</strong>
             
           </h1>
-          <p className='text-slate-300 font-bold text-sm'>Front-End/Back-End Web Developer</p>
+          <p className='text-slate-200 font-bold text-sm'>Front-End/Back-End Web Developer</p>
           <div className='flex gap-4  my-5 justify-center'>
           <Link to= 'https://www.linkedin.com/in/korede-ajibola-2a53981b5?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app' >
-          <img className='w-10' src="https://cdn0.iconfinder.com/data/icons/social-media-free-17/32/linkedin_social_media_logo-1024.png" alt="" />
+          <img className='w-10' src={linkdn} alt="" />
           </Link>
           <Link to = 'https://www.facebook.com/share/k2nrqQQfyHDHrttP/?mibextid=qi2Omg' >
-          <img className='w-10' src="https://cdn0.iconfinder.com/data/icons/social-media-free-17/32/facebook_social_media_logo-1024.png" alt="" />
+          <img className='w-10' src={facebook} alt="" />
           </Link>
           <Link to= 'https://www.instagram.com/_ethanakorede?igsh=YzljYTk1ODg3Zg=='>
-          <img className='w-10' src="https://cdn0.iconfinder.com/data/icons/social-media-free-17/32/instagram_social_media_logo-1024.png" alt="" />
+          <img className='w-10' src={instagram} alt="" />
           </Link>
           <Link to= 'https://x.com/Ethannets?t=k3VZLuV0DOd5hYH5LdOVwA&s=09' >
-          <img className='w-10' src="https://cdn0.iconfinder.com/data/icons/social-media-free-17/32/twitter_social_media_logo-1024.png" alt="" />
+          <img className='w-10' src={twitter} alt="" />
           </Link>
           </div>
       </div>
